67d90dbe00a49e592e3277b2159f1298b7932442
keilaloia <keilaloia@gmail.com>
2016-09-26 16:08:01 -0700

    so many commits just to function

--- MathLib/vec2.h
+++ MathLib/vec2.h
@@ -1,1 +1,15 @@
 #pragma once
+struct vec2
+{
+	float x, y;
+
+};
+
+vec2 operator +(const vec2 &lhs, const vec2 &rhs);
+vec2 operator *(const vec2 &lhs, float rhs);
+vec2 operator *(float rhs, const vec2 &lhs);
+//vec2 operator+(const vec2 &v);
+vec2 operator-(const vec2 &v);
+vec2 &operator+=(vec2 &lhs, const vec2 &rhs);
+
+bool operator==(const vec2 &lhs, const vec2 &rhs);
