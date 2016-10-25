691c15508f17358aa85b55ff63aafe3dc513017d
keilaloia <keilaloia@gmail.com>
2016-10-24 17:59:41 -0700

    dafadf

--- Components/shapedraw.cpp
+++ Components/shapedraw.cpp
@@ -9,8 +9,8 @@ void drawCircle(const Circle & C,unsigned color)
 
 void drawAABB(const AABB &B, unsigned color)
 {
-	sfw::drawLine(B.min().x, B.pos.y, B.max().x, B.pos.y, color);
-	sfw::drawLine(B.min().x, B.pos.y, B.pos.x, B.max().y, color);
+	sfw::drawLine(B.min().x, B.min().y, B.max().x, B.min().y, color);
+	sfw::drawLine(B.min().x, B.min().y, B.min().x, B.max().y, color);
 	sfw::drawLine(B.min().x, B.max().y, B.max().x, B.max().y, color);
 	sfw::drawLine(B.max().x, B.min().y, B.max().x, B.max().y, color);
 }
